990047cfd3150be787287a3b4cb93af570cf6a82
Yori Hadi Putra <49787935+yorihaput@users.noreply.github.com>
2021-11-12 15:05:35 +0700

    Update auto-csrf.js to v.2.0.1
    Add auto csrf to fetch function

--- auto-csrf.js
+++ auto-csrf.js
@@ -1,48 +1,47 @@
 /*
  * auto-csrf.js
- * version: 2.0.0
+ * version: 2.0.1
  * author: Yori Hadi Putra <yori.haput@gmail.com>
  * license: MIT
  * https://github.com/yorihaput/Auto-CSRF-Codeigniter
  */
 ((_r, _af, _ff) => {
     "use strict";
-    if(_r.csrf_name !== undefined) {
+    if (_r.csrf_name !== undefined) {
+        if (_r.csrf_value != undefined) localStorage.setItem(_r.csrf_name, _r.csrf_value);
 
-        if(_r.csrf_value != undefined) localStorage.setItem(_r.csrf_name, _r.csrf_value);
-
-        if(_r.jQuery !== undefined) {
+        if (_r.jQuery !== undefined) {
             _af(_r.csrf_name, _r.csrf_value, _r.jQuery.ajaxPrefilter);
-            _r.jQuery(document).ajaxComplete(function(e, x, s) {
+            _r.jQuery(document).ajaxComplete(function (e, x, s) {
                 localStorage.setItem(_r.csrf_name, x.getResponseHeader('resp-hash'));
             });
         }
         _ff(_r.csrf_name, _r.csrf_value);
-    }else{
+    } else {
         console.error("CSRF_NAME variable undefined please define this variable first");
     }
-})(this, function(_cn, _cv, _ap, _as) {
+})(this, function (_cn, _cv, _ap, _as) {
     "use strict";
-    _ap(function(o){
+    _ap(function (o) {
         if (o.type.toLowerCase() === "post") {
             _cv = localStorage.getItem(_cn);
-            if(o.processData == false && o.data.constructor.name == 'FormData'){
+            if (o.processData == false && o.data.constructor.name == 'FormData') {
                 o.data.append(_cn, _cv);
-            }else{
+            } else {
                 o.data = o.data || "";
-                o.data += o.data?"&":"";
+                o.data += o.data ? "&" : "";
                 o.data += _cn + "=" + encodeURIComponent(_cv);
             }
         }
     });
-}, function(_cn, _cv) {
-    document.addEventListener('DOMContentLoaded', function(e) {
-        for(let el of document.forms) {
-            el.addEventListener('submit', function(e) {
+}, function (_cn, _cv) {
+    document.addEventListener('DOMContentLoaded', function (e) {
+        for (let el of document.forms) {
+            el.addEventListener('submit', function (e) {
                 _cv = localStorage.getItem(_cn);
-                if(typeof el[_cn] != 'undefined') {
-                    el[_cn].value =_cv;
-                }else{
+                if (typeof el[_cn] != 'undefined') {
+                    el[_cn].value = _cv;
+                } else {
                     let x = document.createElement("input");
                     x.setAttribute("name", _cn);
                     x.setAttribute("type", "hidden");
@@ -52,10 +51,24 @@
             });
 
             el._submit = el.submit;
-            el.submit = function() {
+            el.submit = function () {
                 this.dispatchEvent(new Event('submit'));
                 this._submit();
             }
         }
     }, false);
+
+    let _fetch = this.fetch;
+    this.fetch = (input, init = {}) => {
+        if (init.headers["Content-Type"] && init.headers["Content-Type"] == "application/json") {
+            if (init.method && init.method.toLowerCase() == 'post') {
+                try {
+                    let _body = JSON.parse(init.body);
+                    _body[_cn] = _cv;
+                    init.body = JSON.stringify(_body);
+                } catch (error) { console.error(error); }
+            }
+        }
+        return _fetch(input, init);
+    }
 });
